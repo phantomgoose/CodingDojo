26d350143c3b9567ffc6cf5343a9e22876266c18
phantomgoose <alex.txt@gmail.com>
2017-09-14 16:32:13 -0700

    add barcode generator - angular

---
+++ MEAN Stack/Angular/Retro-Barcode-Generator/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+    title = "Retro Barcode Generator";
+    colored_divs: Array<RandomColor> = genRandomColors(10);
+}
+
+class RandomColor {
+    r: number;
+    g: number;
+    b: number;
+    color: string;
+
+    constructor() {
+        this.r = Math.floor(Math.random() * 256);
+        this.g = Math.floor(Math.random() * 256);
+        this.b = Math.floor(Math.random() * 256);
+        this.color = `rgba(${this.r}, ${this.g}, ${this.b}, 1.0)`;
+    }
+}
+
+function genRandomColors(numColors: number): Array<RandomColor> {
+    let res = [];
+    for (let i = 0; i < numColors; i++) {
+        res.push(new RandomColor());
+    }
+    return res;
+}
