10bfa86ef7acdea46b30c40a7ce3c8a1b6bf169c
phantomgoose <alex.txt@gmail.com>
2017-09-18 14:27:53 -0700

    minor PowerLevels fix

--- MEAN Stack/Angular/PowerLevels/src/app/power/power.component.ts
+++ MEAN Stack/Angular/PowerLevels/src/app/power/power.component.ts
@@ -9,8 +9,13 @@ export class PowerComponent implements OnInit {
     // just to generate a for loop
     max_power_level = new Array(100);
     power_level: number;
+    selected_level: number;
 
     constructor() {}
 
     ngOnInit() {}
+
+    onSubmit(){
+      this.power_level = this.selected_level;
+    }
 }
