051214b4f4411a1184d968b8f1701c70512889f1
phantomgoose <alex.txt@gmail.com>
2017-10-03 17:29:41 -0700

    add a bunch of basic c# assignments v2

---
+++ .NET Core/Essentials/Music LINQ/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Regex = System.Text.RegularExpressions;
+using JsonData;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Collections to work with
+            List<Artist> Artists = JsonToFile<Artist>.ReadJson();
+            List<Group> Groups = JsonToFile<Group>.ReadJson();
+
+            //========================================================
+            //Solve all of the prompts below using various LINQ queries
+            //========================================================
+
+            //There is only one artist in this collection from Mount Vernon, what is their name and age?
+
+            Artist artistFromVernon = Artists.Where(artist => artist.Hometown == "Mount Vernon").First();
+            System.Console.WriteLine(artistFromVernon.RealName + " " + artistFromVernon.Age);
+
+
+            //Who is the youngest artist in our collection of artists?
+
+            Artist youngest = Artists.OrderBy(artist => artist.Age).First();
+            System.Console.WriteLine(youngest.Age);
+
+            //Display all artists with 'William' somewhere in their real name
+            string pattern = @"William";
+            Regex.Regex r = new Regex.Regex(pattern);
+            List<Artist> williams = Artists.Where(artist => r.IsMatch(artist.RealName)).ToList();
+
+            //Display the 3 oldest artist from Atlanta
+
+            List<Artist> oldest = Artists.OrderByDescending(artist => artist.Age).Where(artist => artist.Hometown == "Atlanta").Take(3).ToList();
+
+            //(Optional) Display the Group Name of all groups that have members that are not from New York City
+
+            var query = Groups.Join(Artists, group => group.Id, artist => artist.GroupId, (group, artist) => {return new { Group = group, Artist = artist };}).Where(groupAndArtist => groupAndArtist.Artist.Hometown != "New York City").Select(groupAndArtist => groupAndArtist.Group.GroupName).Distinct();
+
+            //(Optional) Display the artist names of all members of the group 'Wu-Tang Clan'
+
+            var query2 =
+                from a in Artists
+                join g in Groups on a.GroupId equals g.Id
+                where g.GroupName == "Wu-Tang Clan"
+                select new {a.RealName};
+            
+            var query3 =
+                from g in Groups
+                where g.GroupName.Length < 8
+                select new {g.GroupName};
+        }
+    }
+}
---
+++ .NET Core/Essentials/Simple CRUD with MySQL/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using DbConnection;
+using System.Collections.Generic;
+
+namespace Simple_CRUD_with_MySQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create("Blobby","aodwija", 7);
+            // Create("Alkex","awdoijawoidj", 5);
+            Update(1, "AR", "Aawoidj", 10);
+            Delete(1);
+        }
+
+        static void Read() {
+            List<Dictionary<string, object>> all = DbConnector.Query("SELECT * FROM users");
+            foreach(Dictionary<string, object> dict in all) {
+                System.Console.Write("Entry: ");
+                foreach (KeyValuePair<string, object> keyVal in dict) {
+                    System.Console.Write(keyVal.Key + "=" + keyVal.Value + ", ");
+                }
+                System.Console.WriteLine();
+            }
+        }
+
+        static void Create(string firstName, string lastName, int favoriteNum) {
+            string query = $"INSERT INTO users (FirstName, LastName, FavoriteNumber) VALUES ('{firstName}', '{lastName}', '{favoriteNum}')";
+            System.Console.WriteLine("Adding user...");
+            DbConnector.Execute(query);
+            Read();
+        }
+
+        static void Update(int id, string firstName, string lastName, int favoriteNum) {
+            string query = $"UPDATE users SET FirstName='{firstName}', LastName='{lastName}', FavoriteNumber='{favoriteNum}' WHERE id={id}";
+            System.Console.WriteLine("Updating user...");
+            DbConnector.Execute(query);
+            Read();
+        }
+
+        static void Delete(int id) {
+            string query = $"DELETE FROM users WHERE id={id}";
+            System.Console.WriteLine("Deleting user...");
+            DbConnector.Execute(query);
+            Read();
+        }
+    }
+}
